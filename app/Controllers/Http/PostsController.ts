8f6efd62b6cec6725c9387ee024cd3830ded5324
atrem13 <mertayoga17.my@gmail.com>
2023-01-31 15:48:38 +0800

    update

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,3 +1,7 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class PostsController {}
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'APp/Models/Post'
+export default class PostsController {
+    public async index({request}: HttpContextContract){
+        const posts = await Post.query().preload('user').preload('forum')
+    }
+}
--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,13 +1,38 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import User from 'App/Models/User'
+import Forum from 'App/Models/Forum'
+import { 
+  BaseModel,
+  column,
+  belongsTo,
+  BelongsTo
+} from '@ioc:Adonis/Lucid/Orm'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column()
+  public forumId: number
+
+  @column()
+  public userId: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @belongsTo(() => Forum)
+  public forum: BelongsTo<typeof Forum>
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
